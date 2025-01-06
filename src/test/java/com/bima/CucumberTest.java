4881510065d74ded079999d3ad7fc84a0358c21e
Bima Rachmat Setiawan <bimarachmatsetiawan@gmail.com>
2025-01-06 19:30:46 +0700

    Update CucumberTest.java

--- src/test/java/com/bima/CucumberTest.java
+++ src/test/java/com/bima/CucumberTest.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         glue = {"com.bima.stepdef"},
         features = {"src/test/resources/features"},
-        plugin = {"pretty","html:reports/cucumber.html", "json:reports/cucumber.json"}
+        plugin = {"pretty","html:reports/index.html", "json:reports/cucumber.json"}
 )
 public class CucumberTest {
 
